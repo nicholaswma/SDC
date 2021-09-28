fd47b9c22b35977a99e8c7c1aefec535061a3950
nicholas ma <nicholas.maa@gmail.com>
2021-09-27 20:37:55 -0700

    setup .env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
+const app = express()
+require('dotenv').config()
 
-
-app.listen(3000, () => {
-  console.log('listening on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
 })
