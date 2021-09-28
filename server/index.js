c8ba2d88352f883700d72e197ed9160582d8abe8
nicholas ma <nicholas.maa@gmail.com>
2021-09-28 08:58:22 -0700

    testing env ignore

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 require('dotenv').config()
 
-//testing
+//testingg
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`)
